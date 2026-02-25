6d587c7c2730b99588a37b0d0754ae7a4d9d5239
Chau Quang <254834274+qcq-r@users.noreply.github.com>
2026-02-25 16:34:06 +1100

    Updated plot code

--- analysis.r
+++ analysis.r
@@ -27,12 +27,12 @@ library(tidyverse)
 # Load each file with a meaningful variable name.
 
 ## Expression data
-genelevel_expr <- read.csv("data/GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv")
-colnames(genelevel_expr)[1] <- c("ensembl_gene_id")
+expr <- read.csv("data/GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv")
+colnames(expr)[1] <- c("ensembl_gene_id") # matches with biomaRt notation
 
 ## Metadata
-genelevel_metadata <- read.csv("data/GSE60450_filtered_metadata.csv")
-colnames(genelevel_metadata)[c(1, 4)] <- c("sample_id", "developmental_stage")
+metadata <- read.csv("data/GSE60450_filtered_metadata.csv")
+colnames(metadata)[c(1, 4)] <- c("sample_id", "developmental_stage")
 
 # Inspect the data -------------------------
 
@@ -41,32 +41,26 @@ colnames(genelevel_metadata)[c(1, 4)] <- c("sample_id", "developmental_stage")
 
 
 ## Expression data
-genelevel_expr %>% summarise(
-  rows = n(),
-  columns = ncol(.)
-)
+dim(expr) # rows = 23735, columns = 14
 
 ## Metadata
-genelevel_metadata %>% summarise(
-  rows = n(),
-  columns = ncol(.)
-)
+dim(metadata) # rows = 12, columns = 4
 
 # Prepare/combine the data for plotting ------------------------
 # How can you combine this data into one data.frame?
 
-## Transpose expression data by sample ID
-genelevel_expr_long <- genelevel_expr %>%
+## Reshape dataframe
+expr_long <- expr %>%
   pivot_longer(cols = -c(ensembl_gene_id, gene_symbol),
                names_to = "sample_id",
                values_to = "expression")
 
 ## Check if sample IDs match
-setequal(genelevel_expr_long$sample_id, genelevel_metadata$sample_id)
+setequal(expr_long$sample_id, metadata$sample_id)
 
-## Combine expression data and metadata by sample ID
-genelevel_data <- genelevel_expr_long %>%
-  left_join(genelevel_metadata, by = "sample_id")
+## Match expression data and metadata by sample ID
+expr_data <- expr_long %>%
+  left_join(metadata, by = "sample_id")
 
 # Plot the data --------------------------
 ## Plot the expression by cell type
@@ -74,51 +68,81 @@ genelevel_data <- genelevel_expr_long %>%
 
 plot_genexpr <- function(data, gene_symbol) {
   
-  # Filter for selected gene
+  # Filter for specified gene
   data_filtered <- data %>%
     filter(gene_symbol == !!gene_symbol)
-  data_filtered$immunophenotype <- as.factor(data_filtered$immunophenotype)
-  # Plot filtered data
+  
+  data_filtered$immunophenotype <- factor(data_filtered$immunophenotype,
+    levels = c("basal cell population", "luminal cell population"),
+    labels = c("basal cells", "luminal cells"))
+  
+  # Convert CPM to logCPM (log2 transformation)
+  data_filtered$logCPM <- log2(data_filtered$expression + 1) # avoids plotting error with CPM values of 0
+  
+  # Boxplot
   ggplot(data_filtered, 
          aes(x = immunophenotype, 
-             y = expression,
-             fill = immunophenotype)) +
-    geom_boxplot(width = 0.4) +
-    geom_jitter(position = position_jitter(width = 0.2),
-                alpha = 0.6,
-                size = 1) +
-    labs(title = paste(gene_symbol, "expression by cell type"),
+             y = logCPM,
+             fill = developmental_stage)) +
+    geom_boxplot(width = 0.4,
+                 position = position_dodge(width = 0.6)) +
+    geom_jitter(aes(colour = developmental_stage),
+      position = position_jitterdodge(jitter.width = 0, dodge.width = 0.6),
+      alpha = 0.6,
+      size = 1,
+      show.legend = FALSE) +
+    ## y-axis breaks
+    scale_y_continuous(
+      breaks = seq(floor(min(data_filtered$logCPM, na.rm = TRUE)),
+      ceiling(max(data_filtered$logCPM, na.rm = TRUE)),
+      by = 0.5)) +
+    
+    ## Axes and plot labels
+    labs(title = bquote(italic(.(gene_symbol))),
          x = "Cell type",
-         y = "Expression (TMM-normalised CPM)") +
+         y = bquote("Normalised expression ("*log[2]*"CPM)"),
+         fill = "Developmental stage") +
+    
+    ## Plot theme
     theme_bw() +
     scale_fill_manual(values = c(
-      "basal cell population" = "indianred2",
-      "luminal cell population" = "deepskyblue2")) +
+      "18.5 day pregnancy" = "indianred2",
+      "2 day lactation" = "deepskyblue2",
+      "virgin" = "chartreuse3")) +
+    scale_colour_manual(values = c(
+      "18.5 day pregnancy" = "indianred2",
+      "2 day lactation" = "deepskyblue2",
+      "virgin" = "chartreuse3")) +
     theme(
-      # x-axis
+      ### x-axis
       axis.text.x = element_text(size = 10, color = "black"),
-      axis.title.x = element_text(face = "bold"),
-      # y-axis
+      ### y-axis
       axis.text.y = element_text(size = 10, color = "black"),
       axis.ticks = element_line(linewidth = 0.8, color = "black"),
-      axis.title.y = element_text(face = "bold"),
-      # Grid
+      ### Grid
       panel.grid.major = element_line(color = "grey90", linewidth = 0.5),
       panel.grid.minor = element_line(color = "grey92"),
-      # Title
-      title = element_text(face = "bold"),
-      # Legend
-      legend.position = "none"
-    )
+      ### Title
+      plot.title = element_text(hjust = 0.5),
+      ### Legend
+      legend.position = "right")
 }
 
 ## Generate plot
-plot1 <- plot_genexpr(genelevel_data, "Gnai3")
+print(plot1 <- plot_genexpr(expr_data, "Gnai3"))
+print(plot2 <- plot_genexpr(expr_data, "Tfe3"))
 
 ## Save the plot
 ### Show code for saving the plot with ggsave() or a similar function
 ggsave(filename = "results/gene expression boxplot_gnai3.png",
        plot = plot1, 
+       width = 2000,
+       height = 2000,
+       units = "px",
+       dpi = 300)
+
+ggsave(filename = "results/gene expression boxplot_Tfe3.png",
+       plot = plot2, 
        width = 2000,
        height = 2000,
        units = "px",
